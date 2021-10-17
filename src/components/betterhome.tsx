8e2596ea2ff7c9df5666004eba4589b5d88ea035
guitarisprettierthanpiano <shfabrizio@gmail.com>
2021-10-17 06:00:36 -0400

    adding a few paragraphs for home page

--- src/components/betterhome.tsx
+++ src/components/betterhome.tsx
@@ -4,6 +4,20 @@ import { NavLink } from 'react-router-dom';
 function ClickBuy(){
     document.querySelector('nav').style.display = "none";
 }
+
+/*  <h2>Some Other Time (Video)</h2>
+ 
+    <iframe 
+    loading='lazy'
+    width="100%" height="100%"
+    src="https://www.youtube.com/embed/JR--I7zmunw" 
+    title="YouTube video player" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    scrolling="no"
+    frameBorder="0">
+
+    </iframe>
+*/
 const BetterHome: React.FC = () => {
     
     return(
@@ -32,19 +46,16 @@ const BetterHome: React.FC = () => {
             </div>
         </div>
 
-        <h2>Some Other Time (Video)</h2>
- 
-        <iframe 
-            loading='lazy'
-            width="100%" height="100%"
-            src="https://www.youtube.com/embed/JR--I7zmunw" 
-            title="YouTube video player" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            scrolling="no"
-            frameBorder="0">
-
-        </iframe>
+    
         
+    <div className='about-album'>
+        <h1>ABOUT CLOUD TASTER</h1>
+        <p>The vibrations of the strings. The sustained ringing of the sound holes. Even the smell of wood polish. This is why I can't leave this instrument alone.</p><br/>
+        <p>This album is just a man with his guitar. Nothing written out and pre-planned. Play through the song once and a second time very freely. Improvise the entire thing. I added a tiny bit of reverb and that is it.</p><br/>
+        <p>This whole Covid-19 thing gave me the time to really sit down and work on my weaknesses on the instrument. It is a tricky instrument that only spending a lot of time with it can you get comfortable with it's peculiarities. Learning to improvise over a song's structure is such a pleasure.</p>
+        <p>The songs I've picked here for some reason or another have stuck to me. </p>
+        <p></p>
+    </div>
     </div>
         
     </>
